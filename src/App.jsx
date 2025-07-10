85ff361d5a0c9d497673ad9cb36f4c139afad44a
uDayGetIT <114350224+uDayGetIT@users.noreply.github.com>
2025-07-10 20:01:55 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CVOptimizer from './components/CVOptimizer'
+
+function App() {
+  return (
+    <div className="App">
+      <CVOptimizer />
+    </div>
+  )
+}
+
+export default App
