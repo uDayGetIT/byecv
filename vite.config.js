a9bd42c6262b971c59e29112a2095e0df1ee7898
uDayGetIT <114350224+uDayGetIT@users.noreply.github.com>
2025-07-10 20:08:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cv-optimizer/', // Replace 'cv-optimizer' with your actual repository name
+  base: '/byecv/', // Replace 'cv-optimizer' with your actual repository name
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
